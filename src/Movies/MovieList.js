7ffefc12bfaf72a0bbca7f31043dc15ec0441d0f
Rylie <rylie.stone2005@gmail.com>
2023-12-31 06:43:37 -0500

    finished challenge given

--- src/Movies/MovieList.js
+++ src/Movies/MovieList.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function MovieList(props) {
+  const navigate = useNavigate()
+  const movieClick = id => () => {
+    navigate(`movies/${id}`)
+  }
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails key={movie.id} movie={movie} movieClick={movieClick(movie.id)}/>
       ))}
     </div>
   );
@@ -14,7 +19,7 @@ function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={props.movieClick}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
